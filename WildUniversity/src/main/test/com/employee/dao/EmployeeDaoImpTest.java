2494230c378f4046580919834ac182e20e05ce74
teja0207 <suryach.j2ee@gmail.com>
2018-07-05 10:32:30 -0400

    added wilduni

---
+++ WildUniversity/src/main/test/com/employee/dao/EmployeeDaoImpTest.java
@@ -0,0 +1,31 @@
+package com.employee.dao;
+
+import junit.framework.TestCase;
+
+public class EmployeeDaoImpTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testAddEmployee() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAllEmployees() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteEmployee() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEmployee() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateEmployee() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ WildUniversity/src/main/test/com/employee/servlets/EmployeeServletTest.java
@@ -0,0 +1,23 @@
+package com.employee.servlets;
+
+import junit.framework.TestCase;
+
+public class EmployeeServletTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testEmployeeServlet() {
+		fail("Not yet implemented");
+	}
+
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
